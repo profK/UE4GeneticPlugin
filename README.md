9a621a895046962795728b7864aa66d6ba3d3df5
Jeff Kesselman <jpkessel@purdue.edu>
2022-03-13 17:17:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GeneomePoolProject
+
+Developed with Unreal Engine 4
---
+++ Source/GeneomePoolProject/GeneomePoolProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GeneomePoolProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENEOMEPOOLPROJECT_API AGeneomePoolProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
