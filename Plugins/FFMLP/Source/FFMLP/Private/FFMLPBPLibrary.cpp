ef34e906eea4ad40d4df8320d29f9e6ae920ffb7
Jeff Kesselman <jpkessel@purdue.edu>
2022-03-15 13:57:12 -0400

    strated neural net plugin

---
+++ Plugins/FFMLP/Source/FFMLP/Private/FFMLPBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FFMLPBPLibrary.h"
+#include "FFMLP.h"
+
+UFFMLPBPLibrary::UFFMLPBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UFFMLPBPLibrary::FFMLPSampleFunction(float Param)
+{
+	return -1;
+}
+
